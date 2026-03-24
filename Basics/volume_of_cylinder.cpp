6d896d44416f08b6dda79c4912e30f3a43e34e40
Krishnendu Maiti <krishnendumaiti227@gmail.com>
2026-03-24 22:42:46 +0530

    volume calculation for a cylinder

---
+++ Basics/volume_of_cylinder.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int radius, height;
+    cout<<"enter radius : ";
+    cin>>radius ;
+    cout<<"enter height : ";
+    cin>>height;
+    cout<<"volume = "<<2*3.1415*radius*height;
+}
