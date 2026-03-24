ed844de50dbefe116fcba64c50740ec1ff00d56b
Krishnendu Maiti <krishnendumaiti227@gmail.com>
2026-03-24 22:44:05 +0530

    Add files via upload

---
+++ Basics/difference_between_ascii.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char first, second;
+    cout<<"enter the 1st letter : ";
+    cin>>first;
+    cout<<"enter the 2nd letter : ";
+    cin>>second;
+    int x=(int)first, y=(int)second;
+    cout<<"difference = "<<y-x;
+}
